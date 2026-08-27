b24503a8bde87681751a9465875d30d179b8dfa9
pbcgodsglory <pbcgodglory@gmail.com>
2026-08-27 15:52:05 -0500

    Finalize hourly display refresh

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
 const slideshow = document.querySelector("iframe");
 
-let currentVersion = null;
-
-async function checkForUpdates() {
-    try {
-        const response = await fetch("version.txt?t=" + Date.now());
-        const newVersion = (await response.text()).trim();
-
-        if (currentVersion === null) {
-            currentVersion = newVersion;
-            return;
-        }
-
-        if (newVersion !== currentVersion) {
-            currentVersion = newVersion;
-
-            slideshow.src = slideshow.src;
-        }
-
-    } catch (error) {
-        console.log("Could not check for display updates.");
-    }
-}
-
-checkForUpdates();
-
-setInterval(checkForUpdates, 30000);
+setInterval(function () {
+    slideshow.src = slideshow.src;
+}, 3600000);
